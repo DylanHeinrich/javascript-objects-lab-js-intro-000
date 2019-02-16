1c4209cf34b81c2b6fc759c24ea0326385b7d45e
Dylan Heinrich <harly13@live.com>
2019-02-16 03:17:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var clone = Object.assign(object, key, value)
-  return clone
+  return Object.assign({}, object, {[key]: value})
 }
