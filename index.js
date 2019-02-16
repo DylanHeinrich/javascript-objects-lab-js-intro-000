b99f0b28ea5b7383ccbda0d004d6fd48d627b821
Dylan Heinrich <harly13@live.com>
2019-02-16 03:27:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object, key) {
   return newObject
 }
 
-function destructivelyDeletFromObjectbyKey(object, key) {
+function destructivelyDeletFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
